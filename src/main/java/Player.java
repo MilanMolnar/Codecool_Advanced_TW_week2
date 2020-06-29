318e087229f5ed3a022c06f35acba8714e1b0d72
Viktor Ferenczi <viktorferenczi@MacBook-Air.local>
2020-06-29 12:10:23 +0200

    Creating a Player based menu.

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Player {
     public void next(){
 
@@ -6,7 +8,33 @@ public class Player {
 
     }
     public void show(){
-
+        System.out.println("Welcome to our music player");
+        Scanner sc = new Scanner(System.in);
+        String back = "";
+        while (back != "b"){
+            String command = sc.next();
+            switch (command){
+                case "start":
+                    start();
+                    break;
+                case "stop":
+                    stop();
+                    break;
+                case "next":
+                    next();
+                    break;
+                case "prev":
+                    prev();
+                    break;
+                case "back":
+                    Main.main(null);
+                    back = "b";
+                    break;
+                default:
+                    System.out.println("anyádot");
+                    break;
+            }
+        }
     }
     public void stop(){
 
